0fe5fed1ecaf2516754bb4e41eb497ea392756ae
“FeReDragon” <f3030ma@gmail.com>
2023-11-02 14:47:38 -0300

    core-modulo-criacao

---
+++ comercio-local/src/app/core/componentes/troca-senha-reset/troca-senha-reset.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-troca-senha-reset',
+  templateUrl: './troca-senha-reset.component.html',
+  styleUrls: ['./troca-senha-reset.component.scss']
+})
+export class TrocaSenhaResetComponent {
+
+}
