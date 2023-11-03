8b20f863ccd3be6c36ba4e853f8351e0ca98a274
“FeReDragon” <f3030ma@gmail.com>
2023-11-03 20:06:04 -0300

    update comercio

--- comercio-local/src/app/comercio/comercio.module.ts
+++ comercio-local/src/app/comercio/comercio.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaComerciosComponent } from './componentes/lista-comercios/lista-comercios.component';
 import { HomeComercioComponent } from './componentes/home-comercio/home-comercio.component';
+import { SharedModule } from "../shared/shared.module";
 
 
 
 
 @NgModule({
-  declarations: [
-    ListaComerciosComponent,
-    HomeComercioComponent
-  ],
-  imports: [
-    CommonModule,
-  ]
+    declarations: [
+        ListaComerciosComponent,
+        HomeComercioComponent
+    ],
+    imports: [
+        CommonModule,
+        SharedModule
+    ]
 })
 export class ComercioModule { }
